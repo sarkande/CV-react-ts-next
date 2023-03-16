8928a77f684ad8394056607b9e14d67eaa6dbdef
Sarkande <aperezgo74@gmail.com>
2023-03-16 11:51:51 +0000

    portfolio

--- src/components/ProjectThumbnail/index.tsx
+++ src/components/ProjectThumbnail/index.tsx
@@ -1,38 +1,53 @@
 import { useState } from 'react'
 import Image from 'next/image'
 import styles from './projectThumbnail.module.scss'
+import ImageWithFallback from '../ImageWithFallback'
 
 interface MyProps {
   children?: React.ReactNode
-  img?: string
+  thumbnail?: string
   title: string
+  carousel?: string[]
 }
 
 export default function ProjectThumbnail({
   children,
-  img,
+  thumbnail,
   title,
+  carousel,
 }: React.PropsWithChildren<MyProps>) {
   const [open, setOpen] = useState(false)
-
+  const [src, setSrc] = useState(thumbnail !== undefined ? thumbnail : '')
   const handleOpen = () => {
     console.log('click')
     setOpen(!open)
   }
-
   return (
     <div className={`${styles.thumbnail} ${open ? styles.maximized : null}`}>
       {open ? (
         <div>
           {children}
-          {img ? (
+          {carousel ? (
             <Image src="/profile.png" alt="13" width={60} height={60} />
           ) : null}
         </div>
       ) : null}
+      {!open ? (
+        <ImageWithFallback
+          className={styles.thumbnail_picture}
+          key={src}
+          alt=""
+          width={200}
+          height={150}
+          src={`/${src} `}
+          fallbackSrc="/project/image_not_available.png"
+        />
+      ) : null}
       <div className={styles.thumbnail_title}>
         <h1>{title}</h1>
-        <button onClick={handleOpen}>{!open ? 'Ouvrir' : 'Fermer'}</button>
+        <button onClick={handleOpen}>
+          {!open ? 'Ouvrir détails' : 'Fermer détails'}
+        </button>
       </div>
     </div>
   )
--- src/pages/portfolio/index.tsx
+++ src/pages/portfolio/index.tsx
@@ -8,30 +8,109 @@ export default function Portfolio() {
       <Head>
         <title>Allan Perez - Portfolio</title>
       </Head>
-      <h1>Portfolio</h1>
+      <h1 className={styles.title}>Formation ReactJS</h1>
       <div className={styles.portfolio}>
-        <ProjectThumbnail title="Foundry VTT Naheulbeuk">
+        <ProjectThumbnail
+          title="Foundry VTT Naheulbeuk"
+          thumbnail="project/naheulbeuk.jpg"
+        >
           gitlab: <a href="">aaa</a>
           <p>
             Projet permettant de rajouter un système donjon de naheulbeuk à
             foundryVTT un logiciel permettant de mettre en place du jeu de rôle
           </p>
         </ProjectThumbnail>
-        <ProjectThumbnail title="Talents-Affinity" img="">
+        <ProjectThumbnail
+          title="Talents-Affinity"
+          thumbnail="project/talents.jpg"
+        >
           <p>Test</p>
         </ProjectThumbnail>
-        <ProjectThumbnail title="Site AngularJS/RubyOnRails DofusTouch" img="">
+        <ProjectThumbnail
+          title="Site AngularJS/RubyOnRails Goldrush"
+          thumbnail="project/goldrush_grand.png"
+        >
           <p>Test</p>
         </ProjectThumbnail>
-        <ProjectThumbnail title="" img="">
+        <ProjectThumbnail title="OrcWar" thumbnail="project/orcwar.png">
           <p>Test</p>
         </ProjectThumbnail>
-        <ProjectThumbnail title="" img="">
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+      </div>
+      <h1 className={styles.title}>Projets</h1>
+      <div className={styles.portfolio}>
+        <ProjectThumbnail
+          title="Foundry VTT Naheulbeuk"
+          thumbnail="naheulbeuk.jpg"
+        >
+          gitlab: <a href="">aaa</a>
+          <p>
+            Projet permettant de rajouter un système donjon de naheulbeuk à
+            foundryVTT un logiciel permettant de mettre en place du jeu de rôle
+          </p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="Talents-Affinity" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail
+          title="Site AngularJS/RubyOnRails DofusTouch"
+          thumbnail=""
+        >
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
           <p>Test</p>
         </ProjectThumbnail>
       </div>
 
-      <h1>Vieux projets</h1>
+      <h1 className={styles.title}>Vieux projets</h1>
+      <div className={styles.portfolio}>
+        <ProjectThumbnail
+          title="Foundry VTT Naheulbeuk"
+          thumbnail="naheulbeuk.jpg"
+        >
+          gitlab: <a href="">aaa</a>
+          <p>
+            Projet permettant de rajouter un système donjon de naheulbeuk à
+            foundryVTT un logiciel permettant de mettre en place du jeu de rôle
+          </p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="Talents-Affinity" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail
+          title="Site AngularJS/RubyOnRails DofusTouch"
+          thumbnail=""
+        >
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+        <ProjectThumbnail title="" thumbnail="">
+          <p>Test</p>
+        </ProjectThumbnail>
+      </div>
     </>
   )
 }
